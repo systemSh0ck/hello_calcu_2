af988ad79ac05d6615254fe3e866a07fc8b07bcb
Bojan Ivanović <bojan.ivanovic6@gmail.com>
2015-11-12 11:01:29 +0100

    Rewriting with separating the code into more classes

--- app/src/main/java/com/schocktopia/systemshock/hellocalcu2/MainActivity.java
+++ app/src/main/java/com/schocktopia/systemshock/hellocalcu2/MainActivity.java
@@ -8,10 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-	String firstOperand = "";
-	String secondOperand = "";
-	String operator = "";
-	String result = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -64,222 +60,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 		switch (v.getId()) {
 			case R.id.number1:
-				PressNumber("1");
 				break;
 			case R.id.number2:
-				PressNumber("2");
 				break;
 			case R.id.number3:
-				PressNumber("3");
 				break;
 			case R.id.number4:
-				PressNumber("4");
 				break;
 			case R.id.number5:
-				PressNumber("5");
 				break;
 			case R.id.number6:
-				PressNumber("6");
 				break;
 			case R.id.number7:
-				PressNumber("7");
 				break;
 			case R.id.number8:
-				PressNumber("8");
 				break;
 			case R.id.number9:
-				PressNumber("9");
 				break;
 			case R.id.number0:
-				PressNumber("0");
 				break;
 			case R.id.operation_plus:
-				if (operator.equals("")) {
-					operator = "+";
-				}
-				//checking to see whether operator was hit more than once
-				if(operationsTV.getText().length() > 2 && (String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("/")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("*")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("-")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("+")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("^")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("sqrt"))){
-					break;
-				}else{
-					operationsTV.append(" + ");
-				}
-				//
-				if(firstOperand.length() <= 0){
-					firstOperand = secondOperand;
-					secondOperand = "";
-					resultTV.setText("");
-				}else if(firstOperand.length() > 0 && secondOperand.length() > 0){
-					result = MathHandler.Calculate(firstOperand, secondOperand, operator);
-					resultTV.setText(result);
-					firstOperand = result;
-					secondOperand = "";
-				}else{
-					break;
-				}
-				operator = "+";
 				break;
 			case R.id.operation_minus:
-				if (operator.equals("")) {
-					operator = "-";
-				}
-				//checking to see whether operator was hit more than once
-				if(operationsTV.getText().length() > 2 && (String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("/")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("*")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("-")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("+")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("^")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("sqrt"))){
-					break;
-				}else{
-					operationsTV.append(" - ");
-				}
-				//
-				if(firstOperand.length() <= 0){
-					firstOperand = secondOperand;
-					secondOperand = "";
-					resultTV.setText("");
-				}else if(firstOperand.length() > 0 && secondOperand.length() > 0){
-					result = MathHandler.Calculate(firstOperand, secondOperand, operator);
-					resultTV.setText(result);
-					firstOperand = result;
-					secondOperand = "";
-				}else{
-					break;
-				}
-				operator = "-";
 				break;
 			case R.id.operation_mult:
-				if (operator.equals("")) {
-					operator = "*";
-				}
-				//checking to see whether operator was hit more than once
-				if(operationsTV.getText().length() > 2 && (String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("/")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("*")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("-")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("+")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("^")
-						|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("sqrt"))){
-					break;
-				}else{
-					operationsTV.append(" * ");
-				}
-				//
-				if(firstOperand.length() <= 0){
-					firstOperand = secondOperand;
-					secondOperand = "";
-					resultTV.setText("");
-				}else if(firstOperand.length() > 0 && secondOperand.length() > 0){
-					result = MathHandler.Calculate(firstOperand, secondOperand, operator);
-					resultTV.setText(result);
-					firstOperand = result;
-					secondOperand = "";
-				}else{
-					break;
-				}
-				operator = "*";
 				break;
 			case R.id.operation_div:
-				if (operator.equals("")) {
-					operator = "/";
-				}
-				//checking to see whether operator was hit more than once
-				if(operationsTV.getText().length() > 2 && (String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("/")
-							|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("*")
-							|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("-")
-							|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("+")
-							|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("^")
-							|| String.valueOf(operationsTV.getText().charAt(
-						operationsTV.getText().length() - 2)).equals("sqrt"))){
-					break;
-				}else{
-					operationsTV.append(" / ");
-				}
-				//
-				if(firstOperand.length() <= 0){
-					firstOperand = secondOperand;
-					secondOperand = "";
-					resultTV.setText("");
-				}else if(firstOperand.length() > 0 && secondOperand.length() > 0){
-					result = MathHandler.Calculate(firstOperand, secondOperand, operator);
-					resultTV.setText(result);
-					firstOperand = result;
-					secondOperand = "";
-				}else{
-					break;
-				}
-				operator = "/";
 				break;
 			case R.id.operation_sqare:
-				if(operator.equals("")){
-					operator = "^";
-				}
-				if(secondOperand.length() > 0){
-					result = MathHandler.Calculate(firstOperand, secondOperand, operator);
-					firstOperand = result;
-					resultTV.setText(result);
-					secondOperand = "";
-				}else if(result.length() > 0){
-					result = MathHandler.Calculate(firstOperand, result, operator);
-					resultTV.setText(result);
-					firstOperand = result;
-					secondOperand = "";
-				}
-				operator = "^";
 				break;
 			case R.id.operation_sqrt:
-
 				break;
 			case R.id.operation_equals:
-
 				break;
 			case R.id.operation_clear:
-				firstOperand = "";
-				secondOperand = "";
-				operator = "";
-				result = "";
-				resultTV.setText("");
-				operationsTV.setText("");
+				break;
 		}
-	}
-
-	private void PressNumber(String str){
-		secondOperand += str;
-		TextView operationsTV = (TextView)findViewById(R.id.operations_view);
-		TextView resultTV = (TextView)findViewById(R.id.result_view);
-		operationsTV.append(str);
-		if(result.length() > 0){
-			resultTV.setText("");
-			firstOperand = result;
-			result = "";
-		}
-		resultTV.append(str);
 	}
 }
